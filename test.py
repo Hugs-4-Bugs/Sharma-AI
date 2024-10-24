32053640c74fd2428a253378794cc6ef4d858fc0
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-10-24 19:49:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+# from flask import Flask, jsonify
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/status', methods=['GET'])
-def status():
-    return jsonify({'message': 'Server is running'})
+# @app.route('/status', methods=['GET'])
+# def status():
+#     return jsonify({'message': 'Server is running'})
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5001)
 
